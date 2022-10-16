075627fa12c087177932787e26d1e19f53525bd5
wilfrad <wilfradribeiro2132@gmail.com>
2022-10-15 20:22:03 -0500

    form carousel, menu-context final

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-const nav_element = document.getElementById('nav-btn-from');
-const form_element = document.getElementById("form-main");
-var nav_element_hidden = true;
-nav_element.addEventListener('click', hide_content);
-
-function hide_content() {
+(function () {
     console.log('si');
-    /*
-    if (nav_element_hidden == false) {
-        form_element.classList.add('hidden');
-        nav_element_hidden == true;
-    } else {
-        form_element.classList.remove('hidden');
-        nav_element_hidden == false;
-    }*/
-}
+})()
